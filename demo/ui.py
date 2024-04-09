7af904a0cc109020f7b442d0e36deefd0532c2db
Katerina Odabasi <lyrisha@gmail.com>
2024-04-09 23:14:19 +0200

    Improve demo UI

--- demo/ui.py
+++ demo/ui.py
@@ -4,5 +4,15 @@ class UI:
         self.model = model
 
     def launch(self):
-        demo = gr.Interface(fn=self.model.predict, inputs="text", outputs="text")
+        demo = gr.Interface(
+            fn=self.model.predict,
+            inputs="textbox",
+            outputs="text",
+            title="Check the mood of the sentence",
+            description="The model was trained on 'imdb' dataset to classify sentences as 'positive' or 'negative' attitude.",
+            examples=[["Great news! My reality check just bounced."],
+                      ["I'm not arguing, I'm just explaining why I'm right."],
+                      ["I love deadlines, especially the whooshing sound they make as they fly by."],
+                      ["Life is like a box of chocolates. It's full of nuts."]]
+        )
         return demo.launch()
